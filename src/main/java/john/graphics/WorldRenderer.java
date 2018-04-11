18800e5365d75eecf1ef7d945e6e1fb24d9f41a9
John Kayne <johnhoganuke2@gmail.com>
2018-04-11 13:43:36 -0400

    Update WorldRenderer.java

--- src/main/java/john/graphics/WorldRenderer.java
+++ src/main/java/john/graphics/WorldRenderer.java
@@ -6,4 +6,21 @@ import javax.swing.*;
 import john.gui.*;
 import john.gui.window.*;
 
-public class WorldRenderer // Ran out of time...
+public class WorldRenderer implements Runnable {
+  public World world;
+  private boolean running = false;
+  private boolean paused = false;
+  private WorldCanvas canvas;
+  
+  public WorldRenderer(World world,WorldCanvas canvas) {
+    this.world = world;this.canvas = canvas;
+  }
+  
+  public void run() {
+    while (running) {
+      if (!paused) {
+        canvas.render();
+      }
+    }
+  }
+}
